e1905e02640d56b1b43b7f9eabfbb3bda4200e6c
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-06-03 14:08:20 +0100

    pass Monday's test

--- coding_problems/01-monday/monday.py
+++ coding_problems/01-monday/monday.py
@@ -1,2 +1,27 @@
 def kebabize(st: str) -> str:
-    pass
+
+    st = list(st)
+    new_list = []
+    for char in st:
+
+        if char.isnumeric():
+            continue
+        elif char.islower():
+            new_list.append(char)
+        elif char.isupper():
+            char = char.lower()
+            new_list.append("-")
+            new_list.append(char)
+
+    if len(new_list) > 0 and new_list[0] == "-":
+        new_list = new_list[1:]
+
+    kebab_casing = "".join(new_list)
+
+    return kebab_casing
+
+
+if __name__ == "__main__":
+    print(kebabize("thisIsATest"))
+
+    print(kebabize("42"))
