60faf907ce06320ca71d1842a3414d2bffafd71e
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-06-04 14:12:26 +0100

    solve Tuesday's problem

--- coding_problems/02-tuesday/tuesday.py
+++ coding_problems/02-tuesday/tuesday.py
@@ -1,2 +1,15 @@
 def length_of_last_word(s: str) -> int:
-    pass
+
+    index = s.rfind(" ")
+
+    while index == len(s) - 1:
+        s = s[:-1]
+        index = s.rfind(" ")
+
+    return len(s[index + 1:])
+
+
+if __name__ == "__main__":
+    print(length_of_last_word("This is a test"))
+
+    print(length_of_last_word("Test number two   "))
