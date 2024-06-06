c9e0d2a68bbb85e90f97af2792dab59a8ad6d20a
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-06-06 13:51:49 +0100

    solve thursdays challenge

--- coding_problems/04-thursday/thursday.py
+++ coding_problems/04-thursday/thursday.py
@@ -1,2 +1,11 @@
 def missing_number(nums: list[int]) -> int:
-    pass
+
+    length = len(nums)
+
+    for number in range(length+1):
+        if not number in nums:
+            return number
+
+
+if __name__ == "__main__":
+    print(missing_number([0, 1]))
